399fc1bac3f2a38e6a52b388dd6a8564495f9c84
Axel <emk.disenos@gmail.com>
2024-09-30 14:08:52 -0300

    add new header responsive - example

--- 1-ropa/example/1.js
+++ 1-ropa/example/1.js
@@ -1,46 +1,9 @@
-var full=false;
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.transform = 'translateX(0)';
+}
 
-$('.imgWrap').hover(function () {
- // $(this).css('width', '36%');
-   $(this).addClass('current');
-  //$(this).siblings().css('width', '16%');  
-   $(this).siblings().addClass('notCurrent');
-}, function () {
-  //$('.imgWrap').css('width', '20%');  
-  $('.imgWrap').siblings().removeClass('notCurrent');
-  $('.imgWrap').removeClass('current');
- });
-
-$('.imgWrap').click(function () {  
-  
-  if(!full){
-    full = true;
-    // $(this).css('width', '100%');
-    $(this).addClass('current-full');
-  //$(this).siblings().css('width', '0%');  
-   $(this).siblings().addClass('notCurrent-full');
-  }
-});
-
-$('.mfp-close').click(function () {
-  full = false;
-  $('.imgWrap').siblings().removeClass('notCurrent-full');
-  $('.imgWrap').removeClass('current-full');
- 
-});
-
-
-$('.gallery').magnificPopup({
-  delegate: 'a',
-  type: 'image',
-  removalDelay: 300,
-  mainClass: 'mfp-fade',
-  gallery: {
-    enabled: true,
-    navigateByImgClick: true,
-    preload: [0,1]
-  }
-});
-
-
-
+function closeSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.transform = 'translateX(100%)';
+}
