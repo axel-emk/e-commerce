7d3143d5f1a4b0ca58d5751626c1db1f02ccecbe
Axel <emk.disenos@gmail.com>
2024-09-30 19:32:05 -0300

    created slider cards

---
+++ 1-ropa/js/card.js
@@ -0,0 +1,25 @@
+let currentIndex = 0;
+const cardsToShow = 4;  // Cambiar dependiendo del número de cards a mostrar según el tamaño de la pantalla
+const cardWidth = 250;
+const totalCards = document.querySelectorAll('.card').length;
+
+function moveSlider(n) {
+    const slider = document.querySelector('.card-slider');
+    const maxIndex = totalCards - cardsToShow;
+
+    // Actualiza el índice actual
+    currentIndex += n;
+
+    // Si el índice es menor a 0, vuelve al principio
+    if (currentIndex < 0) {
+        currentIndex = maxIndex;
+    }
+
+    // Si el índice es mayor al máximo, vuelve al inicio
+    if (currentIndex > maxIndex) {
+        currentIndex = 0;
+    }
+
+    // Desliza las cards
+    slider.style.transform = `translateX(${-currentIndex * cardWidth}px)`;
+}
