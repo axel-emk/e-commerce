f1cd5db39445ea60a7bffeecf25aff5026563fc5
Axel <emk.disenos@gmail.com>
2024-09-30 23:54:21 -0300

    public docs - pages

---
+++ docs/js/header.js
@@ -0,0 +1,14 @@
+// Header
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.transform = 'translateX(0)';
+}
+
+function closeSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.transform = 'translateX(100%)';
+}
+
+
+
+
